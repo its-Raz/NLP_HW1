a9155fe83e3294dbff3637dfa4124cec1919aba9
razbi <razhaim101@gmail.com>
2024-02-07 20:18:56 +0200

    viterbi+Beam

--- inference.py
+++ inference.py
@@ -3,51 +3,69 @@ from tqdm import tqdm
 import numpy as np
 import math
 
-
+MIN_VALUE = -99999
 def memm_viterbi(sentence, pre_trained_weights, feature2id):
     """
     Write your MEMM Viterbi implementation below
     You can implement Beam Search to improve runtime
     Implement q efficiently (refer to conditional probability definition in MEMM slides)
     """
+    print('here')
+    sentence = ['*','*','The', 'successes', 'achieved', 'at', 'Federal', 'Express', ',', 'Union', 'Carbide', 'and', 'Grumman',
+                'suggest', 'that', 'managing', 'speed', 'may', 'be', 'an', 'underutilized', 'source', 'of', 'competitive', 'advantage', '.','~']
+    beam_threshold = 2
     all_tags = feature2id.feature_statistics.tags
+
+
     current_pai = {}
     prev_pai = {}
     bp = {}
     predictions = []
+    pp_tags = ['*'] # S(k-2)
+    p_tags = ['*'] # S(k-1)
+    c_tags = list(all_tags) # Sk
+    p_pairs = get_all_possible_pairs(pp_tags, p_tags)
     for k in range(2,len(sentence)-1):
-        for curr_tag in all_tags:
-            for prev_tag in all_tags:
-                tag_max = ('', 0)
-                for t in all_tags:
-                    history = create_history(t,prev_tag,curr_tag,sentence,k)
-                    q = calculate_q_probability(pre_trained_weights,feature2id,history)
-                    if k == 1:
-                        value = q
-                    else:
-                        value = prev_pai[(prev_tag,curr_tag)]*q
-                    if value > tag_max[1]:
-                        tag_max = (t,value)
-                current_pai[(prev_tag,curr_tag)] = tag_max[1]
-                bp[(k,prev_tag,curr_tag)] = tag_max[0]
-        prev_pai = current_pai
+        c_pairs = get_all_possible_pairs(p_tags, c_tags) # pai(k,u,v) u from S(k-1) c_tag=all tags
+        q = calculate_q_probability(pre_trained_weights, c_tags, p_pairs, sentence, k, feature2id)
+        for pair in c_pairs:
+            prev_tag = pair[0]
+            curr_tag = pair[1]
+            if k == 2:
+                current_pai[('*', curr_tag)] = q[('*','*', curr_tag)]
 
-    # backtracking
-    max_key = max(current_pai, key=current_pai.get)
+            else:
+                    if k == 3:
+                        current_pai[(prev_tag,curr_tag)] = prev_pai[('*',prev_tag)]*q[('*',prev_tag,curr_tag)]
+                    else:
+                        tag_max = ('', MIN_VALUE)
+                        #q_probabilities[(pair[0],pair[1],curr_tag)]
+                        for t in pp_tags:
+                            value = prev_pai[(t,prev_tag)]*q[(t,prev_tag,curr_tag)]
+                            if value > tag_max[1]:
+                                tag_max = (t,value)
+                        current_pai[(prev_tag, curr_tag)] = tag_max[1]
+                        bp[(k-1,prev_tag, curr_tag)] = tag_max[0]
+        sort_curr_pai = sorted(current_pai.items(), key=lambda x: x[1], reverse=True)[:beam_threshold]
+        current_pai = {key: value for key, value in sort_curr_pai}
+        p_pairs = list(current_pai.keys())
+        prev_pai = current_pai.copy()
+        current_pai = {}
+        p_tags = [tup[1] for tup in p_pairs] # S(k-1)
+        pp_tags = [tup[0] for tup in p_pairs]
+    max_key = max(prev_pai, key=prev_pai.get)
     last_tag = max_key[1]
     prev_last_tag = max_key[0]
-    predictions.append(last_tag)
-    predictions.append(prev_last_tag)
-    for k in range (len(sentence)-4,1,-1):
-
-
-
-    return 0
-
-
-
-
-
+    predictions.extend([last_tag, prev_last_tag])
+    for k in range (len(sentence)-3,2,-1):
+        t = bp[(k,prev_last_tag,last_tag)]
+        predictions.append(t)
+        last_tag = prev_last_tag
+        prev_last_tag = t
+    print('hi!')
+    predictions.append('*')
+    predictions.reverse()
+    return predictions
 
 
 
@@ -60,16 +78,28 @@ def create_history(pp_tag,p_tag,c_tag,sentence,index):
     history = [curr_word,c_tag]
     return history
 
-def calculate_q_probability(weights, feature2id, history):
-    # TODO: optimize
-    curr_tag = history[1]
-    numerator = math.exp(feature_weights_inner_product(weights, feature2id, history,curr_tag))
-    all_tags = feature2id.feature_statistics.tags
-    denominator = 0
-    for tag in all_tags:
-        denominator += math.exp(feature_weights_inner_product(weights, feature2id, history, tag))
-    return numerator / denominator
+def calculate_q_probability(weights,tags_3, possible_perv_pairs,sentence,index,feature2id):
+    #tags_1 is pp_tag tags_2 is p_tag
+    q_probabilities = {}
+    denominator = {}
+    for pair in possible_perv_pairs:
+        for curr_tag in tags_3:
+            history = create_history(pair[0],pair[1],curr_tag,sentence,index)
+            value = math.exp(feature_weights_inner_product(weights, feature2id, history,curr_tag))
+            q_probabilities[(pair[0],pair[1],curr_tag)] = value # THIS IS THE NEMRATOR BEFORE DIVISION
+            if pair in denominator:
+                denominator[pair] += value
+            else:
+                denominator[pair] = value
+    for key,value in q_probabilities.items():
+        q_probabilities[key] = value/(denominator[(key[0],key[1])])
 
+    return q_probabilities
+
+def get_all_possible_pairs(tags_1,tags_2):
+    x, y = np.meshgrid(tags_1, tags_2)
+
+    return list(zip(x.flatten(), y.flatten()))
 
 def feature_weights_inner_product(weights, feature2id, history,tag):
     feature_vecotr = create_feature_vector(history, weights.size, feature2id.feature_to_idx,tag)
